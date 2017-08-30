9355bb7acde066fbdc8dba7dbc6575bf5b758e87
Tiago Do Couto <cs.couto@outlook.com>
2017-08-30 17:53:30 -0300

    messages

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -17,21 +17,64 @@ class ChatViewController: UIViewController {
     @IBOutlet var messageTextfield: UITextField!
     @IBOutlet var messageTableView: UITableView!
     
+    //vars
+    var messages = [Message]()
+    
     //system functions
     override func viewDidLoad() {
         super.viewDidLoad()
         messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
+        messageTableView.estimatedRowHeight = 120.0
+        messageTableView.rowHeight = UITableViewAutomaticDimension
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tableviewTapped))
+        messageTableView.addGestureRecognizer(tapGesture)
     }
     
     //actions
     @IBAction func sendPressed(_ sender: AnyObject) {
-
+        messageTextfield.endEditing(true)
+        messageTextfield.isEnabled = false
+        sendButton.isEnabled = false
+        
+        guard let message = messageTextfield.text, message.characters.count > 0  else {
+            return
+        }
+        
+        let messageDB = Database.database().reference().child("Messages")
+        let messageDict = ["Sender": Auth.auth().currentUser?.email, "MessageBody": message]
+        
+        messageDB.childByAutoId().setValue(messageDict){
+            (error, ref) in
+            if error == nil {
+                self.messageTextfield.isEnabled = true
+                self.sendButton.isEnabled = true
+                
+                self.messageTextfield.text = ""
+            }
+        }
     }
     @IBAction func logOutPressed(_ sender: AnyObject) {
         do {
             try Auth.auth().signOut()
             self.navigationController?.popToRootViewController(animated: true)
         }catch{}
+    }
+    
+    //custom actions
+    func tableviewTapped(){
+        messageTextfield.endEditing(true)
+    }
+    func retrieveMessages(){
+        let messageDB = Database.database().reference().child("Messages")
+        messageDB.observe(.childAdded, with: {
+            (snapshot) in
+            let snap = snapshot.value as! Dictionary<String, String>
+            let message = Message()
+            message.message = snap["MessageBody"]!
+            message.sender = snap["Sender"]!
+            self.messages.append(message)
+            self.messageTableView.reloadData()
+        })
     }
 }
 
@@ -40,10 +83,28 @@ extension ChatViewController: UITableViewDelegate, UITableViewDataSource {
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return messages.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customMessagecell", for: indexPath) as! CustomMessageCell
+        cell.messageBody.text = messages[indexPath.row].message
+        cell.senderUsername.text = messages[indexPath.row].sender
+        cell.avatarImageView.image = UIImage(named: "egg")
         return cell
+    }
+}
+
+extension ChatViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.5){
+            self.heightConstraint.constant = 308
+            self.view.layoutIfNeeded()
+        }
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.5){
+            self.heightConstraint.constant = 50
+            self.view.layoutIfNeeded()
+        }
     }
 }
